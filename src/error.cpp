b25c74c01b7ff2236878803bf3697343f11f7cab
andrewilly <italian.hitech@gmail.com>
2026-04-22 17:22:52 +0200

    Create error.cpp

---
+++ src/error.cpp
@@ -0,0 +1,10 @@
+#include "error.h"
+#include <cerrno>
+#include <cstring>
+
+namespace tarc {
+
+// Le funzioni sono già inline nell'header, questo file è per 
+// eventuali implementazioni non-inline future
+
+} // namespace tarc
