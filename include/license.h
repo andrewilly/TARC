3f3b60cb7d04f9bee28ecacd3ed15ad20c7ef808
andrewilly <italian.hitech@gmail.com>
2026-04-22 12:23:19 +0200

    Update license.h

--- include/license.h
+++ include/license.h
@@ -2,21 +2,9 @@
 #include <string>
 
 namespace License {
-
-    // Valida il formato e il checksum della chiave
-    bool is_valid(const std::string& key);
-
-    // Restituisce il path del file licenza (OS-aware)
-    std::string get_license_path();
-
-    // Carica la chiave salvata dal disco
-    std::string load_saved_key();
-
-    // Salva la chiave su disco
-    bool save_key(const std::string& key);
-
-    // Controlla la licenza: carica dal disco o chiede all'utente.
-    // Termina il processo se la licenza non è valida.
+    [[nodiscard]] bool is_valid(const std::string& key);
+    [[nodiscard]] std::string get_license_path();
+    [[nodiscard]] std::string load_saved_key();
+    [[nodiscard]] bool save_key(const std::string& key);
     void check_and_activate();
-
-} // namespace License
+}
