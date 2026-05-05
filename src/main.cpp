e398a9574b1cdea9494eaf2a5e000731e8fa6d06
andrewilly <italian.hitech@gmail.com>
2026-05-05 22:04:01 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -51,7 +51,7 @@ struct Command {
         Version
     } type = None;
     
-    int level = 3;
+    int level = 7;
     bool sfx = false;
     bool flat = false;
     bool force = false;
@@ -94,7 +94,7 @@ static Command parse_args(int argc, char* argv[]) {
     
     if (prefix == "-c") {
         cmd.type = Command::Create;
-        cmd.level = 3;
+        cmd.level = 7;
         
         if (arg == "-cbest") {
             cmd.level = 19;  // Massima compressione: LZMA2 1GB dict + ZSTD 19 ultra
